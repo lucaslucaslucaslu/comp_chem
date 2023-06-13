111be5fa3a44b1ed4d64a1653645d22c7fb977ae
Jiale Lu <lujiale13@gmail.com>
2023-06-13 14:28:25 -0700

    add comments, edit formatting

--- imine/imine_formation.py
+++ imine/imine_formation.py
@@ -2,19 +2,7 @@ import pandas as pd
 from rdkit.Chem import AllChem as Chem
 import os
 
-# Assuming ds_1 and ds_2 are pandas DataFrames containing the datasets
-# ds_1 = pd.DataFrame({
-#     'SMILES': ['CC(C)=O', 'C=O', 'CC(C)C=O'],
-#     'Name': ['Acetone', 'Formaldehyde', 'Isobutyraldehyde']
-# })
-
-# ds_2 = pd.DataFrame({
-#     'SMILES': ['CC(N)C', 'CN', 'CCC(N)C'],
-#     'Name': ['Isopropylamine', 'Methylamine', 'Isobutylamine']
-# })
-
-import pandas as pd
-
+#set of aldehydes and methyl ketones
 ds_1 = pd.DataFrame({
     'SMILES': [
         'CC(=O)C', 'CCC(=O)C', 'CCCC(=O)C', 'CC(C)=O', 'CC(=O)CC',
@@ -32,7 +20,7 @@ ds_1 = pd.DataFrame({
     ]
 })
 
-
+#set of primary amines
 ds_2 = pd.DataFrame({
     'SMILES': [
         'CC(N)C', 'CCC(N)C', 'CC(C)CN', 'CC(C)(C)CN', 'CCCC(N)C',
@@ -48,64 +36,62 @@ ds_2 = pd.DataFrame({
 })
 
 
-
 def form_imine(aldehyde_ketone, amine):
-    # Extract SMILES and names from ds_1 and ds_2
+    #data preprocessing
     aldehyde_ketone_smiles = ds_1.loc[aldehyde_ketone, 'SMILES']
     amine_smiles = ds_2.loc[amine, 'SMILES']
-    
-    # Convert aldehyde/ketone SMILES to RDKit molecule
     aldehyde_ketone_mol = Chem.MolFromSmiles(aldehyde_ketone_smiles)
     
     # Convert amine SMILES to RDKit molecule
     amine_mol = Chem.MolFromSmiles(amine_smiles)
-    
-    # Create a new molecule for the imine intermediate
     imine_mol = Chem.RWMol(aldehyde_ketone_mol)
 
+    #search for C=O
     pattern = Chem.MolFromSmarts('C=O')
     matches = imine_mol.GetSubstructMatches(pattern)
 
+    #set the bond order to 1 (single bond)
     bond_idx = imine_mol.GetBondBetweenAtoms(matches[0][0], matches[0][1])
     bond_idx.SetBondType(Chem.BondType.SINGLE)
 
+    #give oxygen a negative charge
     imine_mol.GetAtomWithIdx(matches[0][1]).SetFormalCharge(-1)
 
     imine_mol.UpdatePropertyCache(strict=False)
 
+    #add the amine part to the same mol object but not yet connect them
     imine_mol = Chem.CombineMols(imine_mol,amine_mol)
     
     # Get the atom index for the nitrogen in the amine
     nitrogen_idx = amine_mol.GetSubstructMatch(Chem.MolFromSmarts('N'))[0] + len(aldehyde_ketone_mol.GetAtoms())
 
+    #change status to an editable mol object
     ed_imine = Chem.EditableMol(imine_mol)
 
+    #connect the carbonyl carbon with the nitrogen
     ed_imine.AddBond(matches[0][0], nitrogen_idx, order=Chem.BondType.SINGLE)
     
-    # Assign a positive charge to the nitrogen
-
+    #change back to mol object
     imine_mol = ed_imine.GetMol()
 
+    # Assign a positive charge to the nitrogen
     imine_mol.GetAtomWithIdx(nitrogen_idx).SetFormalCharge(1)
 
     imine_mol.UpdatePropertyCache(strict=False)  
     
     # Convert the modified imine molecule back to SMILES
     imine_smiles = Chem.MolToSmiles(imine_mol)
-    
     return imine_smiles
 
-# Specify the desired size of the DataFrame
 rows = len(ds_1) * len(ds_2)
 columns = ['ketone_aldehyde', 'amine','SMILES_intermediate_1']
 
-# Create a DataFrame with specified size and column names
 df_res = pd.DataFrame(index=range(rows), columns=columns)
 
 
 for i in range(0, len(ds_1)):
     for j in range(0, len(ds_2)):
-        HOME = os.getcwd()
+        # HOME = os.getcwd()
         aldehyde_ketone_index = i
         amine_index = j
         imine_smiles = form_imine(aldehyde_ketone_index, amine_index)
@@ -113,9 +99,9 @@ for i in range(0, len(ds_1)):
         df_res.at[i * len(ds_2) + j, 'amine'] = ds_2.loc[j, 'SMILES']
         df_res.at[i * len(ds_2) + j, 'SMILES_intermediate_1'] = imine_smiles
 
+        #create xyz files for each amine + aldehyde/ketone + intermediate combo
+        
         # smiles_strings = [ds_1.loc[i, 'SMILES'], ds_2.loc[j, 'SMILES'], imine_smiles]
-
-
         # os.system(f'mkdir -p rxn_{i}_{j}')
         # os.chdir(f'rxn_{i}_{j}')
         # # make xyz_files
